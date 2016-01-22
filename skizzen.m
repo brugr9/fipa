1aceaa04cf111b353d9ce222a625f05c72a9b5a0
Roland Bruggmann <brugr9@bfh.ch>
2016-01-22 20:53:29 +0100

    +skizzen

---
+++ skizzen.m
@@ -0,0 +1,13 @@
+% Discrete surface S for gray-levels g
+[greyMax, greyMaxLoc] = max(I(:));
+[greyMin, greyMinLoc] = min(I(:));
+g = greyMax - greyMin;
+S = zeros(sizeX, sizeY);
+for x = 1:sizeX
+    for y = 1:sizeY
+        S(x,y) = g-1-I(x,y);
+    end;
+end;
+mesh(S)
+colormap(hot)
+figure; imshow(S, []);         axis off; title('Surface');    hold off;
